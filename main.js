4e4d9754cd64b1927d05958c1ceeb804ed77ff92
Kumara023 <guru0203kum@gmail.com>
2023-12-28 17:07:11 +0800

    third commit

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const parallax = document.getElementById("parallax");
 
 window.addEventListener("scroll", function(){
     let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY= offset * 2 + "px";
+    parallax.style.backgroundPositionY= offset * 0.7 + "px";
 })
 
 
-sadsadasdsadsadasd
